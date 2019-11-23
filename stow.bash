1d74a53793ccd1c09f8edd2812bb7e052bd297cc
Anthony Le Cigne <dev@lecigne.net>
2019-11-23 21:28:14 +0100

    [meta] Make stow script verbose

--- stow.bash
+++ stow.bash
@@ -2,4 +2,4 @@
 
 PACKAGES="cmus common-lisp cvim emacs fonts gitconfig gnupg i3 packages password-store rofi stumpwm wallpapers xmodmap zsh"
 
-stow ${PACKAGES} -t ${HOME}
+stow -v ${PACKAGES} -t ${HOME}

2fac4c58a1a90bbc0c0d57403ca9bdd94a249422
Anthony Le Cigne <dev@lecigne.net>
2020-04-11 00:21:45 +0200

    [vscode] Add settings

--- stow.bash
+++ stow.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-PACKAGES="cmus common-lisp cvim emacs fonts gitconfig gnupg i3 packages password-store rofi stumpwm wallpapers xmodmap zsh"
+PACKAGES="cmus common-lisp cvim emacs fonts gitconfig gnupg i3 packages password-store rofi stumpwm vscode wallpapers xmodmap zsh"
 
 stow -v ${PACKAGES} -t ${HOME}

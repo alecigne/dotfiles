bc5daf3977a05b1c1f92f3167cc9dbd11dbc681f
Anthony Le Cigne <dev@lecigne.net>
2020-09-20 00:21:19 +0200

    [guix] Add supported and custom packages

--- packages/.packages/guix-manifest.scm
+++ packages/.packages/guix-manifest.scm
@@ -2,12 +2,17 @@
  '("cmus"
    "feh"
    "emacs"
+   "filezilla"
    ;; glibc-locales is necessary since packages installed via Guix
    ;; will not use the locale data of the host system
    "glibc-locales"
+   "ledger"
    "ncdu"
    "numlockx"
    "password-store"
+   "ranger"
    "redshift"
+   "restic"
    "stow"
+   "tmux"
    "tree"))

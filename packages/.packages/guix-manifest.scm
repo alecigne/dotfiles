881e20589f1c1c24889a938808bd9ee2da0dd26f
Anthony Le Cigne <dev@lecigne.net>
2020-09-26 00:44:32 +0200

    [guix] Add packages
    borg, i3lock, i3lock-fancy, i3status

--- packages/.packages/guix-manifest.scm
+++ packages/.packages/guix-manifest.scm
@@ -1,11 +1,15 @@
 (specifications->manifest
- '("cmus"
+ '("borg"
+   "cmus"
    "feh"
    "emacs"
    "filezilla"
    ;; glibc-locales is necessary since packages installed via Guix
    ;; will not use the locale data of the host system
    "glibc-locales"
+   "i3lock"
+   "i3lock-fancy"
+   "i3status"
    "ledger"
    "ncdu"
    "numlockx"

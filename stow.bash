e42248814262237a7121968cd9e3c83a8005dfbc
Anthony Le Cigne <dev@lecigne.net>
2020-12-26 17:38:58 +0100

    [cmus] Stow with --no-folding

--- stow.bash
+++ stow.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PACKAGES="cmus common-lisp cvim emacs fonts gitconfig i3 ledger password-store rofi stumpwm urxvt wallpapers xmodmap zsh"
-PACKAGES_NO_FOLDING="gnupg vscode"
+PACKAGES="common-lisp cvim emacs fonts gitconfig i3 ledger password-store rofi stumpwm urxvt wallpapers xmodmap zsh"
+PACKAGES_NO_FOLDING="cmus gnupg vscode"
 
 stow -v ${PACKAGES} -t ${HOME}
 stow -v ${PACKAGES_NO_FOLDING} -t ${HOME} --no-folding

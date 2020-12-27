ac0b6f3b73994d84e7b031e3e8946261b13186a4
Anthony Le Cigne <dev@lecigne.net>
2020-12-27 17:44:05 +0100

    [rclone] Add and manage encrypted conf file

--- stow.bash
+++ stow.bash
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+# Decrypt (if necessary)
+
+RCLONE_DIR="rclone/.config/rclone"
+RCLONE_CONF_FILE="${RCLONE_DIR}/rclone.conf"
+RCLONE_GPG_FILE="${RCLONE_DIR}/rclone.conf.gpg"
+
+if [ ! -f "${RCLONE_CONF_FILE}" ]; then
+    gpg -o "${RCLONE_CONF_FILE}" --pinentry-mode loopback -d "${RCLONE_GPG_FILE}"
+fi
+
+# Stow
+
 PACKAGES="common-lisp cvim emacs fonts gitconfig i3 ledger password-store rofi stumpwm urxvt wallpapers xmodmap zsh"
-PACKAGES_NO_FOLDING="cmus gnupg vscode"
+PACKAGES_NO_FOLDING="cmus gnupg rclone vscode"
 
 stow -v ${PACKAGES} -t ${HOME}
 stow -v ${PACKAGES_NO_FOLDING} -t ${HOME} --no-folding

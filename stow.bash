be6d37212a7670ff05880577586d4ed3ccbb610b
Anthony Le Cigne <dev@lecigne.net>
2025-02-28 19:20:35 +0100

    wallpapers: stow wallpapers in no-folding mode

--- stow.bash
+++ stow.bash
@@ -12,8 +12,8 @@ fi
 
 # Stow
 
-PACKAGES="alacritty common-lisp cvim dunst emacs gitconfig i3 irssi ledger password-store rofi stumpwm tmux urxvt wallpapers xmodmap zsh"
-PACKAGES_NO_FOLDING="cmus gnupg rclone vscode"
+PACKAGES="alacritty common-lisp cvim dunst emacs gitconfig i3 irssi ledger password-store rofi stumpwm tmux urxvt xmodmap zsh"
+PACKAGES_NO_FOLDING="cmus gnupg rclone vscode wallpapers"
 
 stow -v ${PACKAGES} -t ${HOME}
 stow -v ${PACKAGES_NO_FOLDING} -t ${HOME} --no-folding

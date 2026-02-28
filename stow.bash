d3a88e37b35e04a6ba07d9ee526e818bc4bda0a3
Anthony Le Cigne <dev@lecigne.net>
2026-02-28 15:15:05 +0100

    rclone: remove obsolete config

--- stow.bash
+++ stow.bash
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-# Decrypt (if necessary)
-
-RCLONE_DIR="rclone/.config/rclone"
-RCLONE_CONF_FILE="${RCLONE_DIR}/rclone.conf"
-RCLONE_GPG_FILE="${RCLONE_DIR}/rclone.conf.gpg"
-
-if [ ! -f "${RCLONE_CONF_FILE}" ]; then
-    gpg -o "${RCLONE_CONF_FILE}" --pinentry-mode loopback -d "${RCLONE_GPG_FILE}"
-fi
-
-# Stow
-
 PACKAGES="alacritty common-lisp cvim dunst emacs gitconfig i3 irssi ledger password-store rofi stumpwm tmux urxvt xmodmap zsh"
-PACKAGES_NO_FOLDING="cmus gnupg rclone vscode wallpapers"
+PACKAGES_NO_FOLDING="cmus gnupg vscode wallpapers"
 
 stow -v ${PACKAGES} -t ${HOME}
 stow -v ${PACKAGES_NO_FOLDING} -t ${HOME} --no-folding
